7a6bcbc26d3c1d795d75104a8593613ea0e06400
mAmineChniti <emin.chniti@esprit.tn>
2026-01-31 20:42:44 +0100

    upd

---
+++ api-gateway/src/main/java/org/springframework/boot/autoconfigure/web/ServerProperties.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.autoconfigure.web;
+
+/**
+ * Compatibility shim for Spring Cloud Gateway expecting ServerProperties.
+ */
+public class ServerProperties {
+}
---
+++ api-gateway/src/main/java/org/springframework/boot/autoconfigure/web/embedded/NettyWebServerFactoryCustomizer.java
@@ -0,0 +1,8 @@
+package org.springframework.boot.autoconfigure.web.embedded;
+
+/**
+ * Compatibility shim for older Spring Cloud Gateway expectations.
+ * Provides an empty placeholder so conditional classloading succeeds.
+ */
+public class NettyWebServerFactoryCustomizer {
+}
