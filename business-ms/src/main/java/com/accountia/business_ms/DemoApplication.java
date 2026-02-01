fdc1f7010f4c79ca90d53618ff80faa3c8837e8a
mAmineChniti <emin.chniti@esprit.tn>
2026-02-01 22:34:16 +0100

    fixed act

--- business-ms/src/main/java/com/accountia/business_ms/DemoApplication.java
+++ business-ms/src/main/java/com/accountia/business_ms/DemoApplication.java
@@ -2,12 +2,12 @@ package com.accountia.business_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.accountia.business.repository")
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- client-ms/src/main/java/com/accountia/client_ms/DemoApplication.java
+++ client-ms/src/main/java/com/accountia/client_ms/DemoApplication.java
@@ -2,12 +2,12 @@ package com.accountia.client_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.accountia.client.repository")
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
