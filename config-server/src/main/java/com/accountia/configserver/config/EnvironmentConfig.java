7ef12fdd47d81e5b71089fd703a6d044ef3f847d
mAmineChniti <emin.chniti@esprit.tn>
2026-02-27 18:34:07 +0100

    config server

---
+++ config-server/src/main/java/com/accountia/configserver/config/EnvironmentConfig.java
@@ -0,0 +1,55 @@
+package com.accountia.configserver.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class EnvironmentConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "secure")
+    public SecureProperties secureProperties() {
+        return new SecureProperties();
+    }
+
+    public static class SecureProperties {
+        private String jwtSecret;
+        private String datasourcePassword;
+        private String rabbitmqPassword;
+        private String redisPassword;
+
+        public String getJwtSecret() {
+            return jwtSecret;
+        }
+
+        public void setJwtSecret(String jwtSecret) {
+            this.jwtSecret = jwtSecret;
+        }
+
+        public String getDatasourcePassword() {
+            return datasourcePassword;
+        }
+
+        public void setDatasourcePassword(String datasourcePassword) {
+            this.datasourcePassword = datasourcePassword;
+        }
+
+        public String getRabbitmqPassword() {
+            return rabbitmqPassword;
+        }
+
+        public void setRabbitmqPassword(String rabbitmqPassword) {
+            this.rabbitmqPassword = rabbitmqPassword;
+        }
+
+        public String getRedisPassword() {
+            return redisPassword;
+        }
+
+        public void setRedisPassword(String redisPassword) {
+            this.redisPassword = redisPassword;
+        }
+    }
+}
