90499568e5cac9ee6dbd2f1aad579a849fd4dc98
Wiem777 <grajawiem@gmail.com>
2026-04-15 21:22:26 +0100

    Fix JPA entity and component scanning packages for Business and Expense services

--- expense-ms/src/main/java/com/accountia/expense_ms/ExpenseApplication.java
+++ expense-ms/src/main/java/com/accountia/expense_ms/ExpenseApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.accountia.expense.client")
+@ComponentScan(basePackages = "com.accountia.expense")
+@EntityScan(basePackages = "com.accountia.expense.model")
 @EnableJpaRepositories(basePackages = "com.accountia.expense.repository")
 public class ExpenseApplication {
     public static void main(String[] args) {
